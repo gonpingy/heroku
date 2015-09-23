c676e10a642a9c20e798110932db9bc81aa66e94
gonpingy <gonpingy@gmail.com>
2015-09-23 22:34:57 +0900

    サンプル追加

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var bird = require('./routes/bird');
+var book = require('./routes/book');
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-    response.send(cool());
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+}
 
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+}
+
+app.get('/example/d', [cb0, cb1], function (req, res, next) {
+  console.log('response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from D!');
+});
+
+app.use('/bird', bird);
+app.use('/book', book);
